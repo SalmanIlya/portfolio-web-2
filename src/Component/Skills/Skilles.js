463f641bfb1e02fbf83a9379f46a703befebcefe
Salamanilyas <95139252+SalmanIlya@users.noreply.github.com>
2022-11-19 19:50:37 +0500

    add-styling

---
+++ src/Component/Skills/Skilles.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import "./style.css"
+const Skilles = () => {
+  return (
+    <div>
+<h1 className='heading text-center heading1'><strong>Skilles</strong></h1>
+<div className='skill w-75 container'>
+<div className='skills bg-light shadow m-2'>
+    <h3><strong>HTML</strong></h3>
+</div>
+<div className='skills bg-light m-2'>
+    <h3><strong>CSS</strong></h3>
+</div>
+<div className='skills bg-light shadow m-2'>
+    <h3><strong>Bootstrap</strong></h3>
+</div>
+<div className='skills bg-light shadow m-2'>
+    <h3><strong>Tailwind-Css</strong></h3>
+</div>
+<div className='skills bg-light shadow m-2'>
+    <h3><strong>Scss</strong></h3>
+</div>
+<div className='skills bg-light shadow m-2'>
+    <h3><strong>React-Bootstrap</strong></h3>
+</div>
+<div className='skills bg-light shadow m-2'>
+    <h3><strong>Javascript</strong></h3>
+</div>
+<div className='skills bg-light shadow m-2'>
+    <h3><strong>React.js</strong></h3>
+</div>
+<div className='skills bg-light shadow  m-2'>
+    <h3><strong>Next.js</strong></h3>
+</div>
+<div className='skills bg-light shadow m-2'>
+    <h3><strong>Redux</strong></h3>
+</div>
+<div className='skills bg-light shadow m-2'>
+    <h3><strong>Node.js</strong></h3>
+</div>
+
+<div className='skills bg-light shadow m-2'>
+    <h3><strong>MySql</strong></h3>
+</div>
+<div className='skills bg-light shadow m-2'>
+    <h3><strong>Mongose</strong></h3>
+</div>
+
+</div>
+
+    </div>
+  )
+}
+
+export default Skilles
