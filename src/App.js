00e80139d2ba30c600810f1cc7e4e29b77d2d8ab
Salamanilyas <95139252+SalmanIlya@users.noreply.github.com>
2022-11-19 19:54:03 +0500

    smaill-updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import NavBar from './Component/Navbar/NavBar';
+import HeroSection from './Component/HeroSection/HeroSection';
+import About from './Component/About/About';
+import Skilles from './Component/Skills/Skilles';
+import Projects from './Component/Projucts/Projects';
 
 function App() {
   return (
 <>
-
+<NavBar/>
+<HeroSection/>
+<About/>
+<Skilles/>
+<Projects/>
 </>
   );
 }
