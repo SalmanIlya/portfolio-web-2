3f91180440eee6654c28e60e5b47976678aff4a3
Salamanilyas <95139252+SalmanIlya@users.noreply.github.com>
2022-12-05 11:21:46 +0500

    changes

--- src/Component/HeroSection/HeroSection.js
+++ src/Component/HeroSection/HeroSection.js
@@ -1,53 +1,64 @@
-import React from 'react'
-import "./style.css"
-import {BsLinkedin ,BsGithub} from "react-icons/bs"
-import {MdEmail} from "react-icons/md"
-import {FiInstagram} from "react-icons/fi"
-import {SiWhatsapp} from "react-icons/si"
-
-import Typed from "typed.js";
-import { useEffect, useRef } from "react";
+import React from "react";
+import "./style.css";
+import { BsLinkedin, BsGithub } from "react-icons/bs";
+import { MdEmail } from "react-icons/md";
+import { FiInstagram } from "react-icons/fi";
+import { SiWhatsapp } from "react-icons/si";
 
 const HeroSection = () => {
-  const el = useRef(null);
-
-  useEffect(() => {
-    const typed = new Typed(el.current, {
-      strings: ["Salaman ilyas","full stack web developer","Salaman ilyas"], // Strings to display
-      // Speed settings, try diffrent values untill you get good results
-      startDelay: 300,
-      typeSpeed: 100,
-      backSpeed: 100,
-      backDelay: 100
-    });
-
-    // Destropying
-    return () => {
-      typed.destroy();
-    };
-  }, []);
   return (
-    <div id='home'>
-     
-      <div className='w-25 h-50'></div>
-        <div className='herosection'>
-            <p>let's built some thing toGetHer</p>
-            <h1><strong>  Hi, I'm <span className='name'>{/* Element to display typing strings */}
-      <span ref={el}></span> </span></strong></h1>
+    <div id="home">
+      <div className="w-25 h-50"></div>
+      <div className="herosection">
+        <p>let's built some thing toGetHer</p>
+        <h1>
+          <strong> Hi, I'm Salman ilyas </strong>
+        </h1>
 
-            <h1><strong> A Full-Stack <span className='name'> Web Developer</span></strong></h1>
-            <p className='text-center'>i'm a full stack web developer. <br/> i can make a webside  in react.js,next.js and also Redux  in frontend and node.js in backend and<br/> for styling i use bootstrap,react-bootstrap,talwind-css,Scss and css </p>
-            <div className='mt-5'>
-                <a href='!#' className='rounded-circle m-2 p-3 px-3  shadow'><BsLinkedin/></a>
-                <a href='!#' className='rounded-circle m-2 p-3 px-3 text-black shadow'><BsGithub/></a>
-                <a href='!#' className='rounded-circle m-2 p-3 px-3 text-warning  shadow'><MdEmail/></a>
-                <a href='!#' className='rounded-circle m-2 p-3 px-3 text-danger shadow'><FiInstagram/></a>
-                <a href='https://wa.me/+92300-0516714' className='rounded-circle m-2 p-3 px-3 text-success shadow'  ><SiWhatsapp/></a>
-            </div>
+        <h1>
+          <strong>
+            {" "}
+            A Full-Stack <span className="name"> Web Developer</span>
+          </strong>
+        </h1>
+        <p className="text-center">
+          i'm a full stack web developer. <br /> i can make a webside in
+          react.js,next.js and also Redux in frontend and node.js in backend and
+          <br /> for styling i use bootstrap,react-bootstrap,talwind-css,Scss
+          and css{" "}
+        </p>
+        <div className="mt-5">
+          <a href="!#" className="rounded-circle m-2 p-3 px-3  shadow">
+            <BsLinkedin />
+          </a>
+          <a
+            href="!#"
+            className="rounded-circle m-2 p-3 px-3 text-black shadow"
+          >
+            <BsGithub />
+          </a>
+          <a
+            href="!#"
+            className="rounded-circle m-2 p-3 px-3 text-warning  shadow"
+          >
+            <MdEmail />
+          </a>
+          <a
+            href="!#"
+            className="rounded-circle m-2 p-3 px-3 text-danger shadow"
+          >
+            <FiInstagram />
+          </a>
+          <a
+            href="https://wa.me/+92300-0516714"
+            className="rounded-circle m-2 p-3 px-3 text-success shadow"
+          >
+            <SiWhatsapp />
+          </a>
         </div>
-
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
--- src/Component/Navbar/NavBar.js
+++ src/Component/Navbar/NavBar.js
@@ -1,70 +1,107 @@
-import React from 'react'
-import "./style.css"
+import React from "react";
+import "./style.css";
 const NavBar = () => {
-  const item=()=>{
-    const element=document.getElementById("")
-    if(element){
-      element.scrollIntoView({behavior:"smooth"})
+  const item = () => {
+    const element = document.getElementById("");
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
     }
-  }
+  };
   return (
-    <div className='bg-white fixed-top '>
-    <nav className="navbar navbar-expand-lg navbar-light container">
-      <p>nav</p>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
+    <div className="bg-white fixed-top ">
+      <nav className="navbar navbar-expand-lg navbar-light container">
+        <p>nav</p>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav ms-auto text-np">
-      <li className="nav-item  active nav-link ps-3 m-1 shadow " onClick={()=>{
-          const element=document.getElementById("home")
-          if(element){
-            element.scrollIntoView({behavior:"smooth"})
-          }
-      }}> Home </li>
-      <li className="nav-item  active nav-link ps-3 m-1 shadow " onClick={()=>{
-          const element=document.getElementById("about")
-          if(element){
-            element.scrollIntoView({behavior:"smooth"})
-          }
-      }}> About </li>
-      <li className="nav-item active nav-link ps-3   m-1 shadow "
-      onClick={()=>{
-        const element=document.getElementById("skill")
-        if(element){
-          element.scrollIntoView({behavior:"smooth"})
-        }
-    }}
-      > Skills </li>
-      <li className="nav-item active nav-link m-1  shadow " onClick={()=>{
-          const element=document.getElementById("project")
-          if(element){
-            element.scrollIntoView({behavior:"smooth"})
-          }
-      }}> Projects </li>
-      <li className="nav-item  active nav-link m-1 shadow " 
-      onClick={()=>{
-        const element=document.getElementById("rusuma")
-        if(element){
-          element.scrollIntoView({behavior:"smooth"})
-        }
-    }}
-      > Rusuma </li>
-      <li className="nav-item  active nav-link m-1  shadow " onClick={()=>{
-          const element=document.getElementById("contact")
-          if(element){
-            element.scrollIntoView({behavior:"smooth"})
-          }
-      }}> Contact </li>
-   
-     
-    </ul>
-  
-  </div>
-</nav>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto text-np">
+            <li
+              className="nav-item  active nav-link ps-3 m-1  "
+              onClick={() => {
+                const element = document.getElementById("home");
+                if (element) {
+                  element.scrollIntoView({ behavior: "smooth" });
+                }
+              }}
+            >
+              {" "}
+              Home{" "}
+            </li>
+            <li
+              className="nav-item  active nav-link ps-3 m-1  "
+              onClick={() => {
+                const element = document.getElementById("about");
+                if (element) {
+                  element.scrollIntoView({ behavior: "smooth" });
+                }
+              }}
+            >
+              {" "}
+              About{" "}
+            </li>
+            <li
+              className="nav-item active nav-link ps-3   m-1  "
+              onClick={() => {
+                const element = document.getElementById("skill");
+                if (element) {
+                  element.scrollIntoView({ behavior: "smooth" });
+                }
+              }}
+            >
+              {" "}
+              Skills{" "}
+            </li>
+            <li
+              className="nav-item active nav-link m-1   "
+              onClick={() => {
+                const element = document.getElementById("project");
+                if (element) {
+                  element.scrollIntoView({ behavior: "smooth" });
+                }
+              }}
+            >
+              {" "}
+              Projects{" "}
+            </li>
+            <li
+              className="nav-item  active nav-link m-1  "
+              onClick={() => {
+                const element = document.getElementById("rusuma");
+                if (element) {
+                  element.scrollIntoView({ behavior: "smooth" });
+                }
+              }}
+            >
+              {" "}
+              Rusuma{" "}
+            </li>
+            <li
+              className="nav-item  active nav-link m-1   "
+              onClick={() => {
+                const element = document.getElementById("contact");
+                if (element) {
+                  element.scrollIntoView({ behavior: "smooth" });
+                }
+              }}
+            >
+              {" "}
+              Contact{" "}
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
